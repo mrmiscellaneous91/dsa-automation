f6876a7ee514f1b148b496d5bb73a01cfc66e35c
Joshua Mitcham <joshuamitcham@MacBook-Air-de-Joshua.local>
2026-01-30 11:28:22 +0100

    fix: add hyperlinks for schedule call and demo in welcome email

--- lib/templates.ts
+++ lib/templates.ts
@@ -11,8 +11,8 @@ export const WELCOME_EMAIL_TEMPLATE = (name: string, email: string, years: numbe
   </p>
 
   <p>To get you onboarding and make sure you are getting the best use 
-  out of the app, you can schedule a call with me directly here. 
-  You can also find a brief demo here.</p>
+  out of the app, you can schedule a call with me directly <a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ08Pg3nbKrHREBVA264AWT4LrFzClMCEr2aM7qVj66ro6liGAOEi1_4eAOwOnpjflwtyrJIobbN">here</a>. 
+  You can also find a brief demo <a href="https://d2-sqh04.eu1.hs-sales-engage.com/Ctc/W+23284/d2-SQh04/JkM2-6qcW6N1vHY6lZ3mzW7yC-943FJ7P8N8npd_vZzBw7W8nfW1r9j4bkLW5ZPKCx1MRj-BW87y15b9gpl1XW3fyD-M1MJyJNV4C_7k7bQrcfMKD9GZp9xtDW2MPgKf4xWHsfN5-yVd1GtLVSN2RGt1JN2_HzW7-S-Pj6_B-C0W3XwXbL6qgNmFW4Srvnn6gNf3ZW7H3lKC7P6td4W1cgNM58LS7DdW7GRg8838lLwRW8NKCfr5jsgG9W8BMDkR47vDLbW2KDcgZ6DGHvnF1P11yc5-TsMZ9T7458258f5T4K-j04">here</a>.</p>
   
   <p>Feel free to message me directly if you have any questions or issues.</p>
   
